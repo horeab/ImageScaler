b2d66e76f2939406a4b78268b9b9bdc0b6df7cd6
Horea <horea.bucerzan@gmail.com>
2020-11-30 00:07:30 +0200

    landscape mode

--- src/main/java/ImageResizeService.java
+++ src/main/java/ImageResizeService.java
@@ -3,11 +3,22 @@ import org.imgscalr.Scalr;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ImageResizeService {
 
-    private static final Color PAD_COLOR = new Color(157, 239, 194);
+    private static final Color PAD_COLOR = new Color(190, 237, 252);
+
+    private static final boolean isLandscape = false;
+    private static final Map<Integer, Color> INDEX_PAD_COLOR = new HashMap<>();
+
+    static {
+//        INDEX_PAD_COLOR.put(3, new Color(192, 249, 185));
+//        INDEX_PAD_COLOR.put(5, new Color(226, 241, 253));
+    }
+
 
     private static final int XS_WIDTH = 1242;
     private static final int XS_HEIGHT = 2688;
@@ -23,20 +34,27 @@ public class ImageResizeService {
 //        List<Language> langs = Arrays.asList(Language.de);
         List<Language> langs = Arrays.asList(Language.values());
         for (Language lang : langs) {
-//            for (int i = 0; i < 5; i++) {
-//                String imgName = lang.name() + i + ".PNG";
-                String imgName = "2" + lang.name() + ".PNG";
-                resizeIPad(resize8(resizeXS(imgName), imgName), imgName);
-//            }
+            for (int i = 0; i <= 5; i++) {
+                ////////////////////STANDARD////////////////////
+                String imgName = lang.name() + i + ".PNG";
+                ////////////////////////////////////////////
+                ////////////////////UNIQUE//////////////////
+//                String imgName = "2" + lang.name() + ".PNG";
+                ////////////////////////////////////////////////
+                resizeIPad(resize8(resizeXS(imgName, i), imgName), imgName, i);
+            }
         }
     }
 
-    private static void resizeIPad(BufferedImage image, String imgName) {
+    private static void resizeIPad(BufferedImage image, String imgName, int index) {
         if (image == null) {
             return;
         }
+        if (isLandscape) {
+            image = Scalr.rotate(image, Scalr.Rotation.CW_90);
+        }
         int padAmount = 220;
-        image = Scalr.pad(image, padAmount, PAD_COLOR);
+        image = Scalr.pad(image, padAmount, INDEX_PAD_COLOR.getOrDefault(index, PAD_COLOR));
         image = Scalr.crop(image, 0, padAmount, image.getWidth(), image.getHeight() - padAmount * 2);
         image = Scalr.resize(image, Scalr.Mode.FIT_EXACT, IPAD_WIDTH, IPAD_HEIGHT);
         saveImg(image, imgName, "scr_ipad");
@@ -47,36 +65,51 @@ public class ImageResizeService {
         if (image == null) {
             return null;
         }
+        if (isLandscape) {
+            image = Scalr.rotate(image, Scalr.Rotation.CW_90);
+        }
         image = Scalr.crop(image, 0, (XS_HEIGHT - _8_HEIGHT) / 2, _8_WIDTH, _8_HEIGHT);
         saveImg(image, imgName, "scr_8");
         System.out.println("8: Resized " + imgName);
+        if (isLandscape) {
+            image = Scalr.rotate(image, Scalr.Rotation.CW_270);
+        }
         return image;
     }
 
-    private static BufferedImage resizeXS(String imgName) {
+    private static BufferedImage resizeXS(String imgName, int index) {
         BufferedImage image = new ImageLoadSaveService().load("../resources/" + "scr_standard/" + imgName);
         if (image == null) {
             return null;
         }
+        if (isLandscape) {
+            image = Scalr.rotate(image, Scalr.Rotation.CW_90);
+        }
         BufferedImage xsBlackLine = new ImageLoadSaveService().load("xsblackline.png");
         int padAmount = 92;
-        image = Scalr.pad(image, padAmount, PAD_COLOR);
+        image = Scalr.pad(image, padAmount, INDEX_PAD_COLOR.getOrDefault(index, PAD_COLOR));
         image = Scalr.crop(image, padAmount, 0, image.getWidth() - padAmount * 2, image.getHeight());
         image = Scalr.resize(image, Scalr.Mode.FIT_EXACT, XS_WIDTH, XS_HEIGHT);
 
-        final BufferedImage finalImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+        BufferedImage finalImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
         Graphics2D g = finalImage.createGraphics();
         g.drawImage(image, 0, 0, null);
         g.drawImage(xsBlackLine, image.getWidth() / 2 - xsBlackLine.getWidth() / 2, image.getHeight() - 40, null);
         g.dispose();
         saveImg(finalImage, imgName, "scr_xs");
         System.out.println("XS: Resized " + imgName);
+        if (isLandscape) {
+            finalImage = Scalr.rotate(finalImage, Scalr.Rotation.CW_270);
+        }
         return finalImage;
     }
 
     static void saveImg(BufferedImage image, String imgName, String scrFolder) {
         String path = "resources/" + scrFolder;
         createDir(path);
+        if (isLandscape) {
+            image = Scalr.rotate(image, Scalr.Rotation.CW_270);
+        }
         new ImageLoadSaveService().save(image, path + "/" + imgName);
     }
 
